ba5a2998ec2658b7ea79976e01b3ac1f390f4824
LoaloaF <49554827+LoaloaF@users.noreply.github.com>
2019-04-18 09:25:47 +0200

    Add files via upload
    new example file compatible with changed function parameters

--- basic_test.py
+++ basic_test.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from DPre import Drivers, Targets, TARGET, config
 
+import os
 
 # prepare input expression table
 expr = pd.read_csv(config.DPRE_PATH + 'test_data/genes_ntc_expression.tsv', sep='\t', 
@@ -38,63 +39,88 @@ c.reorder(['DMV',  'DVB', 'DVI', 'ICM DITCO', 'ICM DTCO', 'ICM ITCO',
 #   preset color file to set default colors (available for `embryonic` and `all`)
 t = TARGET('embryonic', sort=False, colors_from_file=True)
 
-
 # plot the target similarity of the drivers, using the `euclid` method
-print(c)
-t.target_similarity_heatmap(c, 'euclid', proportional=False,  
-
-              reorder_to_max_possible_bar=False, 
-              cluster_targets=True, 
+t.target_similarity_heatmap(c, 'intersect', proportional=True,  
+              reorder_to_required_effect_bar=False, 
+              cluster_targets=False, 
               cluster_drivers=False,
               
               heatmap_width=1, 
               heatmap_height=1, 
                
               heatmap_range=None,
-              max_possible_bar_range=None,
+              required_effect_bar_range=None,
 
-              show_max_possible_bar=True, 
+              show_required_effect_bar=True, 
               show_target_dendrogram=True,
               show_driver_dendrogram=True, 
 
-              drivers_colorbar = True, 
-              targets_colorbar = True,
+              show_drivers_colorbar = True, 
+              show_targets_colorbar = True,
 )
 
-# t.gene_similarity_heatmap(c, 'euclid',
-
-#                                 differential = True,
-#                                 proportional = False, 
-#                                 norm_prop = True,
-#                                 gene_number = None,
-#                                 specific_markergenes = None,
-#                                 custom_target_genelist = None,
+genes = ['Duxf3', 'Gata2', 'Zscan4b', 'Zscan4a', 'Zscan4d', 'Zscan4c', 'Zscan4e', 'Zscan4f', 'Zscan4a', 'Hsp90aa1', 'Hsp90ab1', # 2C genes
+    'Mef2a', 'Mef2b', 'Mef2c', 'Mef2d', 'Utf1', 'Dppa2', 'Dppa3', 'Dppa4', 'Il6st', 'Foxo1', 'Prdm4', 'Zic1', 'Il6st',
+    'Epcam', 'Tdgf1', 'Cldn3', 'Cldn4', 'Ocln', 'Alpl', 'Crb3', 'Cldn11', 'Inadl','Terf1', # 'Initiation'
+    "Gdf3", "Nanog", "Zfp42", "Esrrb", "Prdm1", # 'Maturation'
+    "Utf1", "Dppa2", "Dppa3", "Dppa4", "Pecam1`", "Dnmt3l", "Klf2", "Klf5", # 'Stabilization'
+    'Nr6a1', 'Tbx21', 'Cldn11', 'Six4', "Prx", "gasdfgads", "Hist1h2bp",
+    "Tbx3", 'Nr5a1', 'Nr5a1', 'Nr5a3', 'Nr0b1', 'Gdf3', 'Nr6a1', 'Utf1', 'Lin28a', 'Sall4', 'Fbxo15', 'Snai2',
+    'Pkp1', 'Pkp2', 'Lox', 'Serpine1', 'Thbs1', 'Col6a1', 'Tgfb3', 'Tcf4', 'Ocln', 'Prdm1', 'Tgfb1', 'Klf8', 'Zeb1', 'Zeb2', 'Fn1',
+    'Pecam1', 'Nr6a1', 'Nr5a1', 'Nr5a2', 'Wnt5a', 'Wnt3a', 'Trp63', 'Trp53', 'Bmi1', 'Stat3', 'Lifr', 'Bcl3', 'Socs3', 'Bmp1ra', 'Cdc20', 'Ccnf',
+    'Stat3', 'Fgf4', 'Dnmt3b', 'Notch1', 'Grb2', 'Tcf3', 'Zic2', 'Sall1', 'Lin28a',
+    "Ncor1", "Ncor2", "Snai1", "Thy1", "Acta2", "Acta1", 'Actg1', 'Actg2', "Gapdh", "Actb", "Eef1a1",
+    "Pou5f1", "Sox2", "Nanog", "Cdh1", "Cdh2", "S100a4", "Esrrb", "Klf4", "Myc", "Lin28a",
+    "Pecam1", "Dppa5a", 'Dppa2',
+    "Rcor2", "Lin28b", "Foxo4", "Ccnd3", "Ccnb1", "Cbx1", "Dab2", "Gli1", "Tbx2", "Tgfb2",
+    ]
+cols_init = dict.fromkeys(['Duxf3', 'Gata2', 'Zscan4a', 'Zscan4b', 'Zscan4c', 'Zscan4d', 'Zscan4e', 'Zscan4f', 'Zscan4a', 'Hsp90aa1', 'Hsp90ab1', # 2C genes
+    'Mef2a', 'Mef2b', 'Mef2c', 'Mef2d', 'Esrrb', 'Utf1', 'Dppa2', 'Dppa3', 'Dppa4', 'Il6st', 'Foxo1', 'Prdm4', 'Zic1', 'Il6st',
+    'Epcam', 'Tdgf1', 'Cldn3', 'Cldn4', 'Ocln', 'Alpl', 'Crb3', 'Cldn11', 'Inadl','Terf1'], config.colors[15])
+cols_mat = dict.fromkeys(["Gdf3", "Nanog", "Zfp42", "Esrrb", "Prdm1"], config.colors[16]) 
+cols_stab = dict.fromkeys(["Klf2", "Klf5", 'Nr6a1', 'Tbx21', 'Cldn11', 'Six4', "Prx",
+    "Tbx3", 'Nr5a1', 'Nr5a1', 'Nr5a3', 'Nr0b1', 'Gdf3', 'Nr6a1', 'Utf1', 'Lin28a', 'Sall4', 'Fbxo15', 'Snai2',
+    'Pkp1', 'Pkp2', 'Lox', 'Serpine1', 'Thbs1', 'Col6a1', 'Tgfb3', 'Tcf4', 'Ocln', 'Prdm1', 'Tgfb1', 'Klf8', 'Zeb1', 'Zeb2', 'Fn1',
+    'Pecam1', 'Nr6a1', 'Nr5a1', 'Nr5a2', 'Wnt5a', 'Wnt3a', 'Trp63', 'Trp53', 'Bmi1', 'Stat3', 'Lifr', 'Bcl3', 'Socs3', 'Bmp1ra', 'Cdc20', 'Ccnf',
+    'Stat3', 'Fgf4', 'Dnmt3b', 'Dnmt3l', 'Notch1', 'Grb2', 'Tcf3', 'Zic2', 'Sall1', 'Lin28a',
+    "Ncor1", "Ncor2", "Snai1", "Thy1", "Acta2", "Acta1", 'Actg1', 'Actg2', "Gapdh", "Actb", "Eef1a1",
+    "Pou5f1", "Sox2", "Nanog", "Cdh1", "Cdh2", "S100a4", "Esrrb", "Klf4", "Myc", "Lin28a",
+    "Pecam1", "Dppa5a", 'Dppa2', 
+    "Rcor2", "Lin28b", "Foxo4", "Ccnd3", "Ccnb1", "Cbx1", "Dab2", "Gli1", "Tbx2", "Tgfb2"], config.colors[17])
+genes_colorbar = {**cols_stab, **cols_init, **cols_mat}
+t.gene_similarity_heatmap(c,  'intersect',
+                                differential = True,
+                                proportional = True, 
+                                gene_number = 20,
+                                display_genes = 'increasing',
+                                specific_genes = None,
+                                custom_target_genelist = None,
                                 
-#                                 specific_markergenes_show_none = False,
-#                                 heatmap_range_from_config = False,
-#                                 heatmap_width = None,
-#                                 heatmap_height = None,
+                                heatmap_range = None,
+                                heatmap_width = 1,
+                                heatmap_height = .5,
             
-#                                 show_max_possible_bar = True,
-#                                 reorder_to_max_possible_bar = False,
-#                                 max_possible_bar_range_from_config = False,
+                                show_required_effect_bar = True,
+                                reorder_to_required_effect_bar = True,
+                                required_effect_bar_range = None,
             
-#                                 show_sum_plot = True,
-#                                 sum_plot_xlim_from_config = False,
-#                                 sum_plot_central_metric = 'mean',
+                                show_sum_plot = True,
+                                sum_plot_range = None,
+                                sum_plot_central_metric = 'median',
+                                show_driverlabels_sum_plot = False,
                                 
-#                                 cluster_genes = True,
-#                                 show_gene_dendrogram = True,
-#                                 cluster_drivers = True,
-#                                 show_driver_dendrogram = False,
-#                                 drivers_colorbar = True,
-#                                 genes_colorbar = None,
-#                                 show_colorbar_legend = True,
+                                cluster_genes = False,
+                                show_gene_dendrogram = True,
+                                genes_colorbar = True,
+                                cluster_drivers = True,
+                                show_driver_dendrogram = True,
+                                show_drivers_colorbar = True,
                                 
-#                                 title = True, 
-#                                 show_genelabels = True,
-#                                 show_driverlabels = True,
-#                                 genelabels_space = None,
-#                                 driverlabels_space = None,
-#                                 genelabels_size = .8,
-#                                 filename = 'single_gene_overlap.pdf')
+                                show_genelabels = True,
+                                genelabel_space = None,
+                                genelabel_size = .8,
+                                show_driverlabels = False,
+                                driverlabels_space = None,
+                                title = True, 
+                                show_colorbar_legend = True,
+                                filename = 'gene_similarity_hm')

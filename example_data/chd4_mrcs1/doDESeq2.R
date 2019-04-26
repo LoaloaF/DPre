1669fde35a4c2af84ea5bb5a9ded73fb6c02f9d3
simon <simon.steffens@live.de>
2019-04-26 16:08:11 +0200

    switched contrasts c1 & c2 to have up regulated > 0

--- example_data/chd4_mrcs1/doDESeq2.R
+++ example_data/chd4_mrcs1/doDESeq2.R
@@ -48,15 +48,16 @@ for (i in 1:dim(contrasts)[1]) {
   
   c1 = contrasts[i,]$c1
   c2 = contrasts[i,]$c2
-  res = results(dds, contrast=c("shrna", c1, c2))
+  res = results(dds, contrast=c("shrna", c2, c1))
   
-  write.table(res, file=paste(row.names(contrasts[i,]), ".all.tsv", sep=""), sep="\t", col.names=NA)
+  dir.create("./deseq_res")
+  write.table(res, file=paste("deseq_res/", row.names(contrasts[i,]), ".all.tsv", sep=""), sep="\t", col.names=NA)
 
   res = res[complete.cases(res),] # Filter NAs
   pres = res[res$padj<qval,]
   pres = pres[abs(pres$log2FoldChange) > fc_filter, ] # select fold-change here
-  dn = pres[pres$log2FoldChange > 0 ,] # Yes, this is weird, but in DESeq2 the up and dn are not
-  up = pres[pres$log2FoldChange < -0,] # in the orientation that you might expect.
+  dn = pres[pres$log2FoldChange < 0 ,] 
+  up = pres[pres$log2FoldChange > -0,] 
   
   print(paste("up:", dim(up)[1], "dn:", dim(dn)[1]))
   sub_name = paste(row.names(contrasts[i,]), dim(pres)[1], "DE genes", sep=" ")
@@ -69,7 +70,7 @@ for (i in 1:dim(contrasts)[1]) {
   points(log(dn[, 1]), dn[ ,2], col="green", pch=16, cex=0.9)
   title(row.names(contrasts[i,]), sub=sub_name)
   
-  write.table(pres, file=paste("", row.names(contrasts[i,]), ".all.tsv", sep=""), sep="\t", col.names=NA)
+  # write.table(pres, file=paste("deseq_res/", row.names(contrasts[i,]), ".all.tsv", sep=""), sep="\t", col.names=NA)
   # write.table(up, file=paste("", row.names(contrasts[i,]), ".up.tsv", sep=""), sep="\t", col.names=NA)
   # write.table(dn, file=paste("", row.names(contrasts[i,]), ".dn.tsv", sep=""), sep="\t", col.names=NA)
 }

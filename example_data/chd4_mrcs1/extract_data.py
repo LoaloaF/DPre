5dc0cf9b0a1f6ff006cbc2f89cda4c19185ac584
oaxiom <oaxiom@gmail.com>
2019-04-26 15:04:52 +0800

    ADDED ENST ANNOTATION to chd4 experiment

--- example_data/chd4_mrcs1/extract_data.py
+++ example_data/chd4_mrcs1/extract_data.py
@@ -1,16 +1,25 @@
 
-
+import os
 from glbase3 import *
 
 # extract the data;
 
 expn = expression(filename='GSE108087_knockdowns_expr.csv', format={'name': 0}, expn='column[1:]')
 
+# The table ab ove only has the Gene symbol, we need to add back in the ensg:
+#annot = glload(os.path.expanduser('~/mm10/mm10_ensembl_v95_ensg.glb'))
+#annot = annot.getColumns(['name', 'ensg'])
+#annot.saveTSV('mm10_enst_annotation.tsv', key_order=['ensg', 'name']) # This is included in the repository
+
+annot = genelist('mm10_enst_annotation.tsv', format={'force_tsv': True, 'ensg': 0, 'name':1})
+
 print(expn)
 
 expn = expn.sliceConditions(['shLuc.rep1', 'shLuc.rep2', 'shLuc.rep3',
     'shMcrs1.rep1', 'shMcrs1.rep2',
     'shChd4.rep1', 'shChd4.rep2'])
+
+expn = annot.map(genelist=expn, key='name')
 
 expn = expn.removeDuplicates('name') # R doesn't like duplicates
 

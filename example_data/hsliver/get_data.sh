881feeaf2edd697848f8b595415dba0fafc3069e
oaxiom <oaxiom@gmail.com>
2019-05-03 06:59:32 +0800

    fixed error in hsliver get data

--- example_data/hsliver/get_data.sh
+++ example_data/hsliver/get_data.sh
@@ -1,3 +1,5 @@
 
-wget -c ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE70nnn/GSE70741/suppl/GSE70741_genes_expression.tsv.gz
+wget -c "ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE70nnn/GSE70741/suppl/GSE70741_genes_expression.tsv.gz"
+
 gunzip GSE70741_genes_expression.tsv.gz
+

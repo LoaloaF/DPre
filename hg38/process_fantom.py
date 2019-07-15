f74bb6a646a439fdc2c53a46326ec4c25d003560
simon <simon.steffens@live.de>
2019-07-15 18:45:33 +0200

    hg38 cleanup

--- hg38/process_fantom.py
+++ hg38/process_fantom.py
@@ -3,14 +3,13 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-# os.chdir('hg38')
+os.chdir('hg38')
 
 expr = pd.read_csv('hg38.fantom5.tsv', sep='\t', index_col='enst')
 # one gene is duplicated, the second occurance is droppped: ENST00000433169
 expr.drop(expr.index[expr.index.duplicated()], axis=0, inplace=True)
 ann = pd.read_csv('annotation/mart_export.tsv', sep='\t', index_col='Transcript stable ID')
-print(expr)
-print(ann)
+# print(expr)
 print(
     '\n\n'
 )
@@ -25,17 +24,20 @@ ensg_key = ann.reindex(expr.index)['Gene stable ID'].values
 
 # 11 ensg keys that are not unique:
 dupl_ensg = ensg_key[pd.Index(ensg_key).duplicated()]
-
+print(dupl_ensg)
+# print(expr.loc(0)[dupl_ensg])
 # assign ensg key
 expr.index = ensg_key
 # delete the 11 duplicated genes:
 expr = expr.drop(dupl_ensg, axis=0)
 
 expr = expr.drop(['fantom_name', 'promoter_usage', 'name'], axis=1)
+# expr.to_csv('../preset_targets/human/expression_base.tsv', sep='\t')
 # print(expr)
 
     
-rowwise_sd = True
+rowwise_sd = False
+print(expr)
 expr = np.log2(expr +1)
 expr.columns = pd.MultiIndex.from_product([expr.columns, ['log2']])
 
@@ -52,3 +54,6 @@ expr.rename_axis('ensg', axis=0, inplace=True)
 # expr.to_csv('../preset_targets/human/expression.tsv', sep='\t')
 expr.to_pickle('../preset_targets/human/expression.gzip')
 print(expr)
+# plt.hist(expr.xs('z',1 ,1).values.flatten(), bins=300, density=True)
+# plt.show()
+
